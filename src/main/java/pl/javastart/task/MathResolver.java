9f73eef5037b65c198100cf1d49f55e0fcb2a575
Julka Czarnasiak <czarna13500@gmail.com>
2023-07-09 14:56:14 +0200

    Add a solution

---
+++ src/main/java/pl/javastart/task/MathResolver.java
@@ -0,0 +1,19 @@
+package pl.javastart.task;
+
+public class MathResolver {
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public boolean isOdd(int number) {
+        return number % 2 != 0;
+    }
+
+    public double circleField(double radius) {
+        return Math.PI * radius * radius;
+    }
+
+    public int power(int number) {
+        return number * number;
+    }
+}
